3875838d83a914de81a71a93666cb3a1861cd2ae
daniel <danielcast244@gmail.com>
2025-06-14 11:07:29 -0500

    INICIO DE PROYECTO Y VINCULACION DE PRIMS CON POSTGRESQL

---
+++ src/pages/index.tsx
@@ -0,0 +1,7 @@
+export default function Home() {
+  return (
+    <div className="min-h-screen flex items-center justify-center bg-red-500">
+      <h1 className="text-white text-3xl font-bold">Tailwind está funcionando!!</h1>
+    </div>
+  )
+}
